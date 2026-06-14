25d204fedfc25437c350c3b442d3129f826fc563
seronayon <shinya4869ari@gmail.com>
2026-06-14 09:02:28 +0900

    refactor: update TMDb search merge code based on user specifications

--- prompts/blog/映画データベース充実ワークフロー/TMDb検索_合流コード.js
+++ prompts/blog/映画データベース充実ワークフロー/TMDb検索_合流コード.js
@@ -1,6 +1,7 @@
-const idSearch = $('TMDb検索_ID/Wikidata').isExecuted ? ($('TMDb検索_ID/Wikidata').item?.json || {}) : {};
-const hasIdHit = idSearch.id || idSearch.movie_results?.[0]?.id;
-const titleSearch = $('TMDb検索_タイトル').isExecuted ? ($('TMDb検索_タイトル').item?.json || {}) : {};
+const res1 = $('TMDb検索_ID/Wikidata').isExecuted ? ($('TMDb検索_ID/Wikidata').item?.json || {}) : {};
+const res2 = ($('TMDb検索_タイトル').isExecuted && $('TMDb検索_タイトル').item?.json) ? $('TMDb検索_タイトル').item.json : null;
 
-// IDでヒットしていればそちらを優先、なければタイトル検索結果を使用
-return [{ json: hasIdHit ? idSearch : titleSearch }];
+// タイトル検索が実行された場合はその結果を返し、そうでなければ最初のID/Wikidata検索の結果を返す
+const activeRes = res2 || res1;
+
+return [{ json: activeRes }];
