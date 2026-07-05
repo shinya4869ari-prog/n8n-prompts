4da85a01e44c98815d1a9e568c90ba8d8a45e1b9
seronayon <shinya4869ari@gmail.com>
2026-07-05 16:12:38 +0900

    Fix TMDb join script to flatten Wikidata find results for credits lookup

--- prompts/blog/映画データベース充実ワークフロー/TMDb検索_合流コード.js
+++ prompts/blog/映画データベース充実ワークフロー/TMDb検索_合流コード.js
@@ -1,4 +1,11 @@
-const res1 = $('TMDb検索_ID/Wikidata').isExecuted ? ($('TMDb検索_ID/Wikidata').first()?.json || {}) : {};
+const rawRes1 = $('TMDb検索_ID/Wikidata').isExecuted ? ($('TMDb検索_ID/Wikidata').first()?.json || {}) : {};
+const res1 = JSON.parse(JSON.stringify(rawRes1));
+
+// Wikidata経由の/find結果の場合、movie_results[0]の内容をルートにコピーして平坦化する
+if (res1.movie_results && res1.movie_results.length > 0) {
+  Object.assign(res1, res1.movie_results[0]);
+}
+
 const res2 = ($('TMDb検索_タイトル').isExecuted && $('TMDb検索_タイトル').first()?.json) ? $('TMDb検索_タイトル').first().json : null;
 
 // タイトル検索が実行された場合はその結果を返し、そうでなければ最初のID/Wikidata検索の結果を返す
